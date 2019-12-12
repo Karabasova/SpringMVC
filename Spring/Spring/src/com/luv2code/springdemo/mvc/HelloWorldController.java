8a9594154e341976a0a42c4036fd3ad3f1aacfa7
Karabasova <43435796+Karabasova@users.noreply.github.com>
2019-12-12 10:23:15 +0100

    HelloWorldController.java

--- Spring/Spring/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ Spring/Spring/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -4,22 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-
-	// need a controller method to show the initial HTML form
-	
+public class HelloWorldController {	
 	@RequestMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
-	}
-		
-	// need a controller method to process the HTML form
-	
+	}	
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
-	}
-		
+	}	
 }
 
 
